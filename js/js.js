2b3087e28d1580a690a6d6c0e0a0b840a1c97a74
Angelo <alfanoangeloc@gmail.com>
2018-11-27 15:38:48 +0100

    newchanges

--- js/js.js
+++ js/js.js
@@ -1,3 +1,10 @@
 $('.item').click(function() {
     $(this).toggleClass('active');
   });
+  
+  $(document).ready(function () {
+    $('#loc1').on('show.bs.modal', function (e) {
+        var image = $(e.relatedTarget).attr('src');
+        $(".img-responsive").attr("src", image);
+    });
+});
